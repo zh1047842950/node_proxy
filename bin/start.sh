14dd4cad7479e7471bb982a33b013f0bd393bf20
zhong1047842950 <zhong1047842950@163.com>
2021-01-09 16:56:13 +0800

    v3

--- bin/start.sh
+++ bin/start.sh
@@ -7,7 +7,8 @@
   source /etc/profile
   pm2 -V
   npm install
-  pm2 start --name node_proxy npm -- start
+  node ./src/index.js
+  #pm2 start --name node_proxy npm -- start
   echo "Update node_proxy success!"$now
   # 日志监听，保持容器持续运行不退出
   tail -f $TAILLOG
