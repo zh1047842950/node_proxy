7888b193ba1b288aaaa6ce8aaa01d8ecec4eb459
zhonghongyan@coocaa.com <zhonghongyan@coocaa.com>
2021-01-13 11:24:03 +0800

    去除所有Access-Control-Allow

--- src/index.js
+++ src/index.js
@@ -9,12 +9,13 @@ const express = require('express'),
   FileStreamRotator = require('file-stream-rotator')
 
 app.all('*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', req.headers.origin)
+  // res.header('Access-Control-Allow-Origin', req.headers.origin)
+  // res.header('Access-Control-Allow-Origin', "*")
   //Access-Control-Allow-Headers ,可根据浏览器的F12查看,把对应的粘贴在这里就行
-  res.header('Access-Control-Allow-Headers', 'tk,Content-Type,ost,apv,apt,authorization,x-requested-with,Content-Type')
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTION')
-  res.header('Access-Control-Allow-Credentials', 'true')
-  res.header('Content-Type', 'application/json;charset=utf-8')
+  // res.header('Access-Control-Allow-Headers', 'tk,Content-Type,ost,apv,apt,authorization,x-requested-with,Content-Type')
+  // res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTION')
+  // res.header('Access-Control-Allow-Credentials', 'true')
+  // res.header('Content-Type', 'application/json;charset=utf-8')
   next()
 })
 // 缓存一个月
